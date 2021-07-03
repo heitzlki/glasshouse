74aa4c92643c068be07e9c7a64201f77bd248e68
heitzlki <kirill.heitzler@gmail.com>
2021-07-03 19:53:12 +0200

    ✨ change led with mutation

---
+++ client/src/lib/led.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { mutation } from '@urql/svelte';
+
+	const mutateLed = mutation({
+		query: `
+	    mutation {
+	      led
+	    }
+	  `
+	});
+
+	function changeLed() {
+		mutateLed();
+		console.log('test');
+	}
+</script>
+
+<button on:click={changeLed}>Set LED</button>
