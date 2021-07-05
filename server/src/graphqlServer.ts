16c9d4fc9d25eca67f8a0819dddb27e68602b3b2
heitzlki <kirill.heitzler@gmail.com>
2021-07-05 14:42:12 +0200

    🚧 ⚠️ tmp server changes

---
+++ server/src/graphqlServer.ts
@@ -0,0 +1,19 @@
+import { GraphQLServer } from 'graphql-yoga';
+
+import { genSchema } from '@utils/genSchema';
+// import { authMiddleware } from './utils/authMiddleware';
+
+export async function setupServer(redis: any) {
+  const server = new GraphQLServer({
+    // middlewares: [authMiddleware],
+    schema: genSchema(),
+    context: (ctx) => ctx,
+    // context: ({ request }) => ({
+    //   redis,
+    //   url: request.protocol + '://' + request.get('host'),
+    //   session: request.session,
+    //   req: request,
+    // }),
+  });
+  return server;
+}
