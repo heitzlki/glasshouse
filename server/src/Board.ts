194f9ec578f469b5c2aee6c45db26e4fc1c7b06c
heitzlki <kirill.heitzler@gmail.com>
2021-07-18 19:23:51 +0200

    🎨 split up board sensor, stripe and pump

--- server/src/Board.ts
+++ server/src/Board.ts
@@ -1,16 +1,14 @@
-import app from '@index';
-
 import chalk from 'chalk';
-import cron from 'node-cron';
-import moment from 'moment';
-import { Board as JohnnyFiveBoard, Pin, Sensor } from 'johnny-five';
+import { Board as JohnnyFiveBoard } from 'johnny-five';
 
 import { BoardModel } from '@models/board/board.model';
 import { IBoardDocument } from '@models/board/board.types';
 import { SensorModel } from '@models/sensor/sensor.model';
 import { PumpModel } from '@models/pump/pump.model';
 
-import reMap from '@utils/reMap';
+import Sensor from '@board/Sensor';
+import Stripe from '@board/Stripe';
+import Pump from '@board/Pump';
 
 export type optionsType = {
   id?: number | string;
@@ -36,9 +34,9 @@ export default class Board {
       pump: 13,
     },
     public sensor?: Sensor,
-    public stripe?: Pin,
-    public pump?: Pin,
-    public sensorInterval: number = 3000,
+    public stripe?: Stripe,
+    public pump?: Pump,
+
     public previousBoard?: IBoardDocument,
     public currentBoard?: IBoardDocument
   ) {}
@@ -59,9 +57,9 @@ export default class Board {
             )
           );
 
-          this.sensor = new Sensor(this.pins.sensor);
-          this.stripe = new Pin(this.pins.stripe);
-          this.pump = new Pin(this.pins.pump);
+          this.sensor = new Sensor(this.pins.sensor, this.currentBoard);
+          this.stripe = new Stripe(this.pins.stripe, this.currentBoard);
+          this.pump = new Pump(this.pins.pump, this.currentBoard);
         }
       });
     }
@@ -81,22 +79,22 @@ export default class Board {
               port: this.board?.port || '',
             },
             sensor: {
-              connected: this.sensor?.id ? true : false,
-              pin: String(this.sensor?.pin) || '',
+              connected: this.sensor?.sensor.id ? true : false,
+              pin: String(this.sensor?.sensor.pin) || '',
               interval: this.previousBoard.sensor.interval,
             },
             pump: {
-              connected: this.pump?.id ? true : false,
-              pin: String(this.pump?.pin || ''),
-              active: this.pump?.value ? true : false,
+              connected: this.pump?.pump.id ? true : false,
+              pin: String(this.pump?.pump.pin || ''),
+              active: this.pump?.pump.value ? true : false,
               sensor: this.previousBoard.pump.sensor,
               refill: this.previousBoard.pump.refill,
               schedule: this.previousBoard.pump.schedule,
             },
             stripe: {
-              connected: this.stripe?.id ? true : false,
-              pin: String(this.stripe?.pin || ''),
-              active: this.stripe?.value ? true : false,
+              connected: this.stripe?.stripe.id ? true : false,
+              pin: String(this.stripe?.stripe.pin || ''),
+              active: this.stripe?.stripe.value ? true : false,
               schedule: this.previousBoard.stripe.schedule,
             },
           });
@@ -107,196 +105,23 @@ export default class Board {
               port: this.board?.port || '',
             },
             sensor: {
-              connected: this.sensor?.id ? true : false,
-              pin: String(this.sensor?.pin || ''),
+              connected: this.sensor?.sensor.id ? true : false,
+              pin: String(this.sensor?.sensor.pin || ''),
             },
             pump: {
-              connected: this.pump?.id ? true : false,
-              pin: String(this.pump?.pin || ''),
-              active: this.pump?.value ? true : false,
+              connected: this.pump?.pump.id ? true : false,
+              pin: String(this.pump?.pump.pin || ''),
+              active: this.pump?.pump.value ? true : false,
             },
             stripe: {
-              connected: this.stripe?.id ? true : false,
-              pin: String(this.stripe?.pin || ''),
-              active: this.stripe?.value ? true : false,
+              connected: this.stripe?.stripe.id ? true : false,
+              pin: String(this.stripe?.stripe.pin || ''),
+              active: this.stripe?.stripe.value ? true : false,
             },
           });
         }
       });
     }
-  }
-
-  reMapSensorData(value: number) {
-    /**
-     * * reMap Sensor data explanation:
-     * * https://www.arduinoplatform.com/arduino-sensors/detecting-moisture-with-soil-moisture-sensor-and-arduino/
-     *
-     * * Custom values:
-     * * Air: 800
-     * * Water: 355
-     */
-    if (this.currentBoard) {
-      return reMap(
-        value,
-        this.currentBoard.sensor.calibration.air, // replace the value with value when placed in air
-        this.currentBoard.sensor.calibration.water, // replace the value with value when placed in water
-        0,
-        100
-      ).toFixed(0);
-    }
-  }
-
-  async storeSensor() {
-    let sensorIntervalDB = await BoardModel.findOne({ sort: { _id: -1 } });
-    this.sensorInterval = sensorIntervalDB
-      ? sensorIntervalDB.sensor.interval
-      : this.sensorInterval;
-
-    setInterval(async () => {
-      if (this.sensor && app.socket.io) {
-        let valueInPercent = this.reMapSensorData(this.sensor.value);
-        app.socket.send('sensor', valueInPercent);
-        await SensorModel.create({
-          humidity: valueInPercent,
-        });
-      }
-    }, this.sensorInterval);
-  }
-
-  async setStripe(
-    activate: boolean = false,
-    event: string = 'stripe',
-    message: string = 'stopped stripe'
-  ) {
-    if (this.stripe) {
-      activate ? this.stripe.high() : this.stripe.low();
-    }
-
-    if (app.socket.io) {
-      app.socket.io.send(event, message);
-    }
-
-    if (this.currentBoard) {
-      this.currentBoard =
-        (await BoardModel.findOneAndUpdate(
-          {
-            _id: this.currentBoard._id,
-          },
-          {
-            $set: {
-              'stripe.active': activate,
-            },
-          },
-          { new: true }
-        )) || this.currentBoard;
-    }
-  }
-
-  stripeSchedule() {
-    // Checks every 5 sec if the stripe needs to be activated
-    setInterval(() => {
-      if (this.currentBoard && this.currentBoard.stripe.schedule.active) {
-        let timeFormat = 'HH:mm:ss';
-        let isBetween = moment().isBetween(
-          moment(this.currentBoard?.stripe.schedule.from, timeFormat),
-          moment(this.currentBoard?.stripe.schedule.to, timeFormat)
-        );
-
-        if (isBetween) {
-          this.setStripe(true, 'stripe', 'started stripe');
-        } else {
-          this.setStripe(); // disables the stripe by default
-        }
-      }
-    }, 5000);
-    // Checks every minute if the stripe needs to be activated
-    // cron.schedule('* * * * *', async () => {
-
-    // });
-  }
-
-  async setPump(
-    activate: boolean = false,
-    event: string = 'pump',
-    message: string = 'stopped pump'
-  ) {
-    if (this.pump) {
-      activate ? this.pump.high() : this.pump.low();
-    }
-
-    if (app.socket.io) {
-      app.socket.io.send(event, message);
-    }
-
-    if (this.currentBoard) {
-      this.currentBoard =
-        (await BoardModel.findOneAndUpdate(
-          {
-            _id: this.currentBoard._id,
-          },
-          {
-            $set: {
-              'pump.active': activate,
-            },
-          },
-          { new: true }
-        )) || this.currentBoard;
-    }
-  }
-
-  pumpSchedule() {
-    if (this.currentBoard && this.currentBoard.pump.schedule.active) {
-      cron.schedule(this.currentBoard.pump.schedule.cron, () => {
-        this.setPump(true, 'pump', 'started pump');
-
-        if (this.currentBoard)
-          setTimeout(
-            this.setPump, // disables the pump by default
-            Number(this.currentBoard.pump.schedule.duration)
-          );
-      });
-    }
-  }
-
-  activatePump() {
-    // Checks every 5 sec if the pump needs to be activated
-    setInterval(async () => {
-      if (this.currentBoard && this.currentBoard.pump.sensor.active) {
-        let sensorData = await SensorModel.find({
-          timestamp: {
-            $gte:
-              new Date().getTime() - this.currentBoard.pump.sensor.average.for,
-          },
-        });
-
-        let sensorAverage = Number(
-          (
-            sensorData.reduce(
-              (sum: number, data: { timestamp: Date; humidity: number }) => {
-                return sum + Number(data.humidity);
-              },
-              0
-            ) / sensorData.length
-          ).toFixed(0)
-        );
-
-        if (sensorAverage < this.currentBoard.pump.sensor.average.value) {
-          this.setPump(true, 'pump', 'started pump');
-
-          if (this.currentBoard) {
-            setTimeout(
-              this.setPump,
-              Number(this.currentBoard.pump.schedule.duration)
-            );
-          }
-        }
-      }
-    }, 5000);
-
-    // Checks every minute if the pump needs to be activated
-    // cron.schedule('* * * * *', async () => {
-
-    // });
   }
 
   async deleteData() {
